12aa29c7fcabd1961def841834b735213ca49926
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2023-01-22 11:11:19 +0900

    main
    main

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { OktaConfigService } from "../shared/okta/okta-config.service";
 import { OktaWidgetService } from '../shared/okta/okta-widget.service';
 import { MenuListService } from '../shared/menu-list/menu-list.service';
 import { PrimeNGConfig } from 'primeng/api';
+import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -22,6 +23,9 @@ export class DashboardComponent {
   mainAppMenu = [];
   selectedMessage: any;
   myAccessToken;
+  strSafeSite: SafeResourceUrl;
+  strSite='https://kent-nagao-oie.oktapreview.com/app/UserHome?iframe=true&iframeControlHideAll=true&iframeControlHideSearch=true';
+
   constructor(
     private OktaGetTokenService: OktaGetTokenService,
     private OktaSDKAuthService: OktaSDKAuthService,
@@ -29,28 +33,30 @@ export class DashboardComponent {
     private OktaWidgetService: OktaWidgetService,
     private MenuListService: MenuListService,
     private primengConfig: PrimeNGConfig,
+    private sanitizer: DomSanitizer,
   ) { }
 
    async ngOnInit() {
-    // this.strUserSession = await this.authService.isAuthenticated();
-    // switch (this.strUserSession == true) {
-    //   case false: {
-    //     window.location.replace(this.OktaConfigService.strPostLogoutURL);
-    //     break;
-    //   }
-    //   case true: {
-    //     this.strThisUser = await this.authService.token.getUserInfo()
-    //       .then(function (user) {
-    //         return user
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //         window.location.replace(this.OktaConfigService.strPostLogoutURL);
-    //       })
-    //       this.strFullName = await this.strThisUser.name;
-    //       this.myAccessToken = await this.OktaGetTokenService.GetAccessToken()
-    //     break;
-    //   }
-    // }
+    this.strUserSession = await this.authService.isAuthenticated();
+    switch (this.strUserSession == true) {
+      case false: {
+        window.location.replace(this.OktaConfigService.strPostLogoutURL);
+        break;
+      }
+      case true: {
+        this.strThisUser = await this.authService.token.getUserInfo()
+          .then(function (user) {
+            return user
+          })
+          .catch((err) => {
+            console.log(err);
+            window.location.replace(this.OktaConfigService.strPostLogoutURL);
+          })
+          this.strFullName = await this.strThisUser.name;
+          this.myAccessToken = await this.OktaGetTokenService.GetAccessToken()
+          this.strSafeSite = this.sanitizer.bypassSecurityTrustResourceUrl(this.strSite);
+        break;
+      }
+    }
   }
 }
